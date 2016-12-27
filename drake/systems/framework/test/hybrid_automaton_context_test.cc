641256c705d9d26445d76e95239a8e0718071a22
Jonathan DeCastro <jonathan.decastro@tri.global>
2016-12-27 16:20:15 -0500

    Add a stub for the HybridAutomatonContext unit test

---
+++ drake/systems/framework/test/hybrid_automaton_context_test.cc
@@ -0,0 +1,208 @@
+#include "drake/systems/framework/hybrid_automaton_context.h"
+
+#include <stdexcept>
+#include <vector>
+
+#include <Eigen/Dense>
+#include "gtest/gtest.h"
+
+#include "drake/common/eigen_matrix_compare.h"
+#include "drake/systems/framework/basic_vector.h"
+#include "drake/systems/framework/leaf_context.h"
+#include "drake/systems/framework/system.h"
+#include "drake/systems/framework/system_input.h"
+#include "drake/systems/primitives/integrator.h"
+#include "drake/systems/primitives/zero_order_hold.h"
+
+namespace drake {
+namespace systems {
+namespace {
+
+constexpr int kSize = 1;
+constexpr int kNumSystems = 2;
+constexpr double kTime = 12.0;
+
+class HybridAutomatonContextTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    integrator0_.reset(new Integrator<double>(kSize));
+    integrator1_.reset(new Integrator<double>(kSize));
+
+    context_.reset(new HybridAutomatonContext<double>(kNumSystems));
+    context_->set_time(kTime);
+
+    ModalSubsystem<double>* ms0 = AddModalSubsystem(*integrator0_, 0);
+    ModalSubsystem<double>* ms1 = AddModalSubsystem(*integrator1_, 1);
+    DRAKE_DEMAND(ms0 != nullptr);
+    DRAKE_DEMAND(ms1 != nullptr);
+
+    context_->ExportInput(0 /* either integrator's input */, 0 /* port 0 */);
+
+    // Default the initial active ModalSubsystem to be integrator0_.
+    context_->MakeState(0);
+    ContinuousState<double>* xc = context_->get_mutable_continuous_state();
+    xc->get_mutable_vector()->SetAtIndex(0, 42.0);
+    AbstractState* xa = context_->get_mutable_abstract_state();
+    const int size_xa = xa->size();  // TODO(jadecastro): Seems a bit silly.
+    // Sets the modal subsystem to be integrator0_.
+    xa->get_mutable_abstract_state(size_xa-1).SetValue<
+      ModalSubsystem<double>*>(ms0);
+  }
+
+  ModalSubsystem<double>*
+  AddModalSubsystem(System<double>& sys, int mode_id) {
+    auto subcontext = sys.CreateDefaultContext();
+    auto suboutput = sys.AllocateOutput(*subcontext);
+    ModalSubsystem<double> modal_subsystem = ModalSubsystem<double>(
+        mode_id, &sys);
+    context_->
+        AddModalSubsystem(
+            &modal_subsystem, std::move(subcontext), std::move(suboutput));
+    return &modal_subsystem;
+  }
+
+  // Mocks up a descriptor that's sufficient to read a FreestandingInputPort
+  // connected to @p context at @p index.
+  static const BasicVector<double>* ReadVectorInputPort(
+      const Context<double>& context, int index) {
+    SystemPortDescriptor<double> descriptor(nullptr, kInputPort, index,
+                                            kVectorValued, 0);
+    return context.EvalVectorInput(nullptr, descriptor);
+  }
+
+  std::unique_ptr<HybridAutomatonContext<double>> context_;
+  std::unique_ptr<Integrator<double>> integrator0_;
+  std::unique_ptr<Integrator<double>> integrator1_;
+};
+
+// Tests that subsystems have outputs and contexts in the
+// HybridAutomatonContext.
+TEST_F(HybridAutomatonContextTest, RetrieveConstituents) {
+  // The current active ModalSubsystem should be a leaf System.
+  auto context = dynamic_cast<const LeafContext<double>*>(
+      context_->GetSubsystemContext());
+  EXPECT_TRUE(context != nullptr);
+
+  auto output = dynamic_cast<const LeafSystemOutput<double>*>(
+      context_->GetSubsystemOutput());
+  EXPECT_TRUE(output != nullptr);
+}
+
+// Tests that the time writes through to the context of the active
+// ModalSubsystem.
+TEST_F(HybridAutomatonContextTest, Time) {
+  context_->set_time(42.0);
+  EXPECT_EQ(42.0, context_->get_time());
+  for (int i = 0; i < kNumSystems; ++i) {
+    EXPECT_EQ(42.0, context_->GetSubsystemContext()->get_time());
+  }
+}
+
+/*
+// Tests that state variables appear in the diagram context, and write
+// transparently through to the constituent system contexts.
+TEST_F(HybridAutomatonContextTest, State) {
+  // Each integrator has a single continuous state variable.
+  ContinuousState<double>* xc = context_->get_mutable_continuous_state();
+  EXPECT_EQ(2, xc->size());
+  EXPECT_EQ(0, xc->get_generalized_position().size());
+  EXPECT_EQ(0, xc->get_generalized_velocity().size());
+  EXPECT_EQ(2, xc->get_misc_continuous_state().size());
+
+  // The zero-order hold has a difference state vector of length 1.
+  DiscreteState<double>* xd = context_->get_mutable_discrete_state();
+  EXPECT_EQ(1, xd->size());
+  EXPECT_EQ(1, xd->get_discrete_state(0)->size());
+
+  // Changes to the diagram state write through to constituent system states.
+  // - Continuous
+  ContinuousState<double>* integrator0_xc =
+      context_->GetMutableSubsystemContext(2)->get_mutable_continuous_state();
+  ContinuousState<double>* integrator1_xc =
+      context_->GetMutableSubsystemContext(3)->get_mutable_continuous_state();
+  EXPECT_EQ(42.0, integrator0_xc->get_vector().GetAtIndex(0));
+  EXPECT_EQ(43.0, integrator1_xc->get_vector().GetAtIndex(0));
+  // - Discrete
+  DiscreteState<double>* hold_xd =
+      context_->GetMutableSubsystemContext(4)->get_mutable_discrete_state();
+  EXPECT_EQ(44.0, hold_xd->get_discrete_state(0)->GetAtIndex(0));
+
+  // Changes to constituent system states appear in the diagram state.
+  // - Continuous
+  integrator1_xc->get_mutable_vector()->SetAtIndex(0, 1000.0);
+  EXPECT_EQ(1000.0, xc->get_vector().GetAtIndex(1));
+  // - Discrete
+  hold_xd->get_mutable_discrete_state(0)->SetAtIndex(0, 1001.0);
+  EXPECT_EQ(1001.0, xd->get_discrete_state(0)->GetAtIndex(0));
+}
+
+// Tests that the pointers to substates in the HybridAutomatonState are equal to
+// the substates in the subsystem contexts.
+TEST_F(HybridAutomatonContextTest, HybridAutomatonState) {
+  auto diagram_state = dynamic_cast<HybridAutomatonState<double>*>(
+      context_->get_mutable_state());
+  ASSERT_NE(nullptr, diagram_state);
+  for (int i = 0; i < kNumSystems; ++i) {
+    EXPECT_EQ(context_->GetMutableSubsystemContext(i)->get_mutable_state(),
+              diagram_state->get_mutable_substate(i));
+  }
+}
+
+// Tests that no exception is thrown when connecting a valid source
+// and destination port.
+TEST_F(HybridAutomatonContextTest, ConnectValid) {
+*/
+//  EXPECT_NO_THROW(context_->Connect({0 /* adder0_ */, 0 /* port 0 */},
+//                                    {1 /* adder1_ */, 1 /* port 1 */}));
+/*
+}
+
+// Tests that input ports can be assigned to the HybridAutomatonContext and then
+// retrieved.
+TEST_F(HybridAutomatonContextTest, SetAndGetInputPorts) {
+  ASSERT_EQ(2, context_->get_num_input_ports());
+  context_->FixInputPort(0, BasicVector<double>::Make({128}));
+  EXPECT_EQ(128, ReadVectorInputPort(*context_, 0)->get_value()[0]);
+  EXPECT_EQ(256, ReadVectorInputPort(*context_, 1)->get_value()[0]);
+}
+
+TEST_F(HybridAutomatonContextTest, Clone) {
+*/
+  //context_->Connect({0 /* adder0_ */, 0 /* port 0 */},
+  //                  {1 /* adder1_ */, 1 /* port 1 */});
+/*
+  AttachInputPorts();
+
+  std::unique_ptr<HybridAutomatonContext<double>> clone(
+      dynamic_cast<HybridAutomatonContext<double>*>(
+          context_->Clone().release()));
+  ASSERT_TRUE(clone != nullptr);
+
+  // Verify that the time was copied.
+  EXPECT_EQ(kTime, clone->get_time());
+
+  // Verify that the state was copied.
+  // - Continuous
+  const ContinuousState<double>* xc = clone->get_continuous_state();
+  EXPECT_EQ(42.0, xc->get_vector().GetAtIndex(0));
+  EXPECT_EQ(43.0, xc->get_vector().GetAtIndex(1));
+  // - Discrete
+  const BasicVector<double>* xd_vec = clone->get_discrete_state(0);
+  EXPECT_EQ(44.0, xd_vec->GetAtIndex(0));
+
+  // Verify that the cloned input ports contain the same data,
+  // but are different pointers.
+  EXPECT_EQ(2, clone->get_num_input_ports());
+  for (int i = 0; i < 2; ++i) {
+    const BasicVector<double>* orig_port = ReadVectorInputPort(*context_, i);
+    const BasicVector<double>* clone_port = ReadVectorInputPort(*clone, i);
+    EXPECT_NE(orig_port, clone_port);
+    EXPECT_TRUE(CompareMatrices(orig_port->get_value(), clone_port->get_value(),
+                                1e-8, MatrixCompareType::absolute));
+  }
+}
+*/
+
+}  // namespace
+}  // namespace systems
+}  // namespace drake
