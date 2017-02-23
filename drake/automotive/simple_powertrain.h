bc6cdf515484ccf354b64c2e01c6534841cf6aa6
Jonathan DeCastro <jonathan.decastro@tri.global>
2017-02-23 14:04:48 -0500

    Address first round feature review

--- drake/automotive/simple_powertrain.h
+++ drake/automotive/simple_powertrain.h
@@ -7,20 +7,21 @@ namespace automotive {
 
 /// SimplePowertrain -- A simple powertrain system modeled as a first-order lag,
 /// with input taken as the throttle setting and the output taken as the applied
-/// lumped force from the vehicle to the road.
+/// lumped force from the wheels of a vehicle to the road.
 ///
 /// Input:
 ///  - A unitless scalar value representing the throttle input to the power
 ///    system.
 ///
 /// Output:
-///  - The force transmitted from the vehicle to the road [N].
+///  - The force transmitted from the wheels to the road [N].
 ///
 /// @tparam T The vector element type, which must be a valid Eigen scalar.
 ///
 /// Instantiated templates for the following kinds of T's are provided:
 /// - double
 /// - AutoDiffXd
+/// - drake::symbolic::Expression
 ///
 /// They are already available to link against in the containing library.
 ///
@@ -32,15 +33,13 @@ class SimplePowertrain : public systems::LinearSystem<T> {
 
   /// Constructs a simple powertrain model, specified via a fixed time-constant
   /// and scalar gain.  The inputs are as follows:
-  /// @param time_constant is the rise time of the first-order lag [s].
-  /// @param gain is the gain converting throttle input to force output [N].
-  SimplePowertrain(const double time_constant, const double gain)
-      : systems::LinearSystem<T>(Vector1d(-1. / time_constant),
-                                 Vector1d(gain),
-                                 Vector1d(1. / time_constant),
-                                 Vector1d(0.)),
-        time_constant_(time_constant),
-        gain_(gain) {}
+  /// @p time_constant is the rise time of the first-order lag [s].
+  /// @p gain is the gain converting throttle input to force output [N].
+  SimplePowertrain(const double& time_constant, const double& gain)
+      : systems::LinearSystem<T>(make_singleton_matrix(-1. / time_constant),
+                                 make_singleton_matrix(gain),
+                                 make_singleton_matrix(1. / time_constant),
+                                 make_singleton_matrix(0.)) {}
   ~SimplePowertrain() override = default;
 
   const systems::InputPortDescriptor<T>& get_throttle_input_port() const {
@@ -51,15 +50,12 @@ class SimplePowertrain : public systems::LinearSystem<T> {
     return systems::System<T>::get_output_port(0);
   }
 
-  /// Accessors for the system constants.
-  /// @{
-  double get_time_constant() const { return time_constant_; }
-  double get_gain() const { return gain_; }
-  /// @}
-
  private:
-  const double time_constant_{};
-  const double gain_{};
+  static Eigen::MatrixXd make_singleton_matrix(const double& value) {
+    Eigen::MatrixXd matrix(1, 1);
+    matrix << value;
+    return matrix;
+  }
 };
 
 }  // namespace automotive
