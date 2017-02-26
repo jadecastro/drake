d7a98fea1f94090af148b8ac83ce3f50d2420985
Jon <jonathan.decastro@tri.global>
2017-02-25 23:36:29 -0500

    WIP

---
+++ drake/automotive/idm_controller.cc
@@ -0,0 +1,147 @@
+#include "drake/automotive/idm_controller.h"
+
+#include <algorithm>
+#include <cmath>
+#include <utility>
+
+#include <Eigen/Geometry>
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/symbolic_formula.h"
+
+namespace drake {
+
+using systems::rendering::PoseVector;
+
+namespace automotive {
+
+template <typename T>
+IdmController<T>::IdmController(const T& v_ref) : v_ref_(v_ref) {
+  // TODO(jadecastro): Remove v_ref from the constructor.
+  // The reference velocity must be strictly positive.
+  DRAKE_ASSERT(v_ref > 0);
+
+  const int kEgoCarOutputVectorSize = 2;
+  const int kAgentCarOutputVectorSize = 2;
+  const int kLinearAccelerationSize = 1;
+  // Declare the ego car input port.
+  this->DeclareInputPort(systems::kVectorValued, kEgoCarOutputVectorSize);
+  // Declare the agent car input port.
+  this->DeclareInputPort(systems::kVectorValued, kAgentCarOutputVectorSize);
+  // Declare the output port.
+  this->DeclareOutputPort(systems::kVectorValued, kLinearAccelerationSize);
+}
+
+template <typename T>
+IdmController<T>::~IdmController() {}
+
+template <typename T>
+const systems::InputPortDescriptor<T>& IdmController<T>::get_ego_input() const {
+  return systems::System<T>::get_input_port(0);
+}
+
+template <typename T>
+const systems::InputPortDescriptor<T>& IdmController<T>::get_agent_input()
+    const {
+  return systems::System<T>::get_input_port(1);
+}
+
+template <typename T>
+void IdmController<T>::DoCalcOutput(const systems::Context<T>& context,
+                                 systems::SystemOutput<T>* output) const {
+  // Obtain the input/output structures we need to read from and write into.
+  const systems::VectorBase<T>* ego_vector_input =
+      this->EvalAbstractInput(context, this->get_ego_input().get_index());
+  DRAKE_ASSERT(ego_vector_input != nullptr);
+  const PoseVector<T>* const ego_pose =
+      dynamic_cast<const PoseVector<T>*>(ego_vector_input);
+  DRAKE_ASSERT(ego_pose != nullptr);
+
+  // TODO(jadecastro): Make this PoseBundle and select a certain PoseVector.
+  const systems::VectorBase<T>* agents_vector_input =
+      this->EvalAbstractInput(context, this->get_agent_input().get_index());
+  DRAKE_ASSERT(agents_vector_input != nullptr);
+  const PoseVector<T>* const agents_pose =
+      dynamic_cast<const PoseVector<T>*>(agents_vector_input);
+  DRAKE_ASSERT(agents_pose != nullptr);
+
+  systems::BasicVector<T>* const output_vector =
+      output->GetMutableVectorData(0);
+  DRAKE_ASSERT(output_vector != nullptr);
+  const DrivingCommand<T>* const command =
+      dynamic_cast<const DrivingCommand<T>*>(output_vector);
+  DRAKE_ASSERT(command != nullptr);
+
+  // Obtain the parameters.
+  const int kParamsIndex = 0;
+  const IdmPlannerParameters<T>& params =
+      this->template GetNumericParameter<IdmPlannerParameters>(context,
+                                                               kParamsIndex);
+
+  ImplDoCalcOutput(ego_pose, agents_pose, params, command);
+}
+
+template <typename T>
+void IdmController<T>::ImplDoCalcOutput(const PoseVector<T>& ego_pose,
+                                        const PoseVector<T>& agents_pose,
+                                        const IdmPlannerParameters<T>& params,
+                                        DrivingCommand<T>* command) const {
+  Eigen::Translation<double, 3> ego_position = ego_pose.get_translation();
+  const T& x_ego = ego_position.translation().x();
+  const T& v_ego = 0.;  // TODO(jadecastro): Require velocity from SimpleCar.
+  Eigen::Translation<double, 3> agent_position = agents_pose.get_translation();
+  const T& x_agent = agent_position.translation().x();
+  const T& v_agent = 0.;  // TODO(jadecastro): Require velocity from SimpleCar.
+
+  // Ensure that we are supplying the planner with sane parameters and
+  // input values.
+  DRAKE_DEMAND(a > 0.0);
+  DRAKE_DEMAND(b > 0.0);
+  DRAKE_DEMAND(x_agent > (l_a + x_ego));
+
+  const T lead_range = x_agent - x_ego;
+  const T closing_velocity = v_ego - v_agent;
+
+  command->set_throttle(
+      IdmModel(net_distance, closing_velocity, v_ego, params));
+}
+
+template <typename T>
+std::unique_ptr<systems::BasicVector<T>> IdmController<T>::AllocateOutputVector(
+    const systems::OutputPortDescriptor<T>& descriptor) const {
+  DRAKE_DEMAND(descriptor.get_index() <= 1);
+  return std::make_unique<DrivingCommand<T>>();
+}
+
+template <typename T>
+std::unique_ptr<systems::Parameters<T>> IdmController<T>::AllocateParameters()
+    const {
+  auto params = std::make_unique<IdmControllerParameters<T>>();
+  return std::make_unique<systems::Parameters<T>>(std::move(params));
+}
+
+template <typename T>
+void IdmController<T>::SetDefaultParameters(
+    const systems::LeafContext<T>& context,
+    systems::Parameters<T>* params) const {
+  // Default values from https://en.wikipedia.org/wiki/Intelligent_driver_model.
+  auto idm_params = dynamic_cast<IdmControllerParameters<T>*>(
+      params->get_mutable_numeric_parameter(0));
+  DRAKE_DEMAND(idm_params != nullptr);
+  idm_params->set_v_ref(v_ref_);         // desired velocity in free traffic.
+  idm_params->set_a(T(1.0));             // max acceleration.
+  idm_params->set_b(T(3.0));             // comfortable braking deceleration.
+  idm_params->set_s_0(T(1.0));           // minimum desired net distance.
+  idm_params->set_time_headway(T(0.1));  // desired headway to lead vehicle.
+  idm_params->set_delta(T(4.0));  // recommended choice of free-road exponent.
+  idm_params->set_l_a(T(4.5));    // length of leading car.
+}
+
+// These instantiations must match the API documentation in
+// idm_planner.h.
+template class IdmController<double>;
+template class IdmController<drake::TaylorVarXd>;
+template class IdmController<drake::symbolic::Expression>;
+
+}  // namespace automotive
+}  // namespace drake
---
+++ drake/automotive/idm_controller.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <memory>
+
+#include "drake/automotive/gen/driving_command.h"
+#include "drake/automotive/gen/idm_planner_parameters.h"
+#include "drake/common/drake_copyable.h"
+#include "drake/systems/framework/leaf_system.h"
+#include "drake/systems/rendering/pose_vector.h"
+
+namespace drake {
+namespace automotive {
+
+/// IdmController -- an IDM (Intelligent Driver Model) planner.
+///
+/// IDM: Intelligent Driver Model:
+///    https://en.wikipedia.org/wiki/Intelligent_driver_model
+///
+/// Instantiated templates for the following kinds of T's are provided:
+/// - double
+/// - drake::TaylorVarXd
+/// - drake::symbolic::Expression
+///
+/// They are already available to link against in the containing library.
+///
+/// @ingroup automotive_systems
+///
+/// Inputs:
+///   Port 0:
+///      @p pose_ego PoseBundle for the ego car.
+///   Port 1:
+///      @p pose_agent PoseBundle for the traffic cars.
+/// Outputs:
+///   Port 0:
+///      @p vdot_ego linear acceleration of the ego car (scalar) [m/s^2].
+template <typename T>
+class IdmController : public systems::LeafSystem<T> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(IdmController)
+
+  /// @p v_ref desired velocity of the ego car in units of m/s.
+  explicit IdmController(const T& v_ref);
+  ~IdmController() override;
+
+  /// Returns the port to the ego car input subvector.
+  const systems::InputPortDescriptor<T>& get_ego_input() const;
+  const systems::InputPortDescriptor<T>& get_agent_input() const;
+
+  // System<T> overrides.
+  // The output of this system is an algebraic relation of its inputs.
+  bool has_any_direct_feedthrough() const override { return true; }
+
+  std::unique_ptr<systems::Parameters<T>> AllocateParameters() const override;
+
+  void SetDefaultParameters(const systems::LeafContext<T>& context,
+                            systems::Parameters<T>* params) const override;
+
+  static T IdmModel(const T& net_distance,
+                    const T& closing_velocity,
+                    const T& ego_velocity,
+                    const IdmPlannerParameters<T>& params) {
+    const T& v_ref = params.v_ref();
+    const T& a = params.a();
+    const T& b = params.b();
+    const T& s_0 = params.s_0();
+    const T& time_headway = params.time_headway();
+    const T& delta = params.delta();
+    const T& l_a = params.l_a();
+
+    const T position_deficit =
+        ego_velocity * closing_velocity / (2 * sqrt(a * b));
+
+    return a * (1. - pow(ego_velocity / v_ref, delta) -
+                pow((s_0 + ego_velocity * time_headway + position_deficit) /
+                    (net_distance - l_a), 2.));
+  };
+
+ private:
+  void DoCalcOutput(const systems::Context<T>& context,
+                    systems::SystemOutput<T>* output) const override;
+
+  void ImplDoCalcOutput(const systems::rendering::PoseVector<T>& ego_pose,
+                        const systems::rendering::PoseVector<T>& agents_pose,
+                        const IdmPlannerParameters<T>& params,
+                        DrivingCommand<T>* output) const;
+
+  const T v_ref_;  // Desired vehicle velocity.
+};
+
+}  // namespace automotive
+}  // namespace drake
