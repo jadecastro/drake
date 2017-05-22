e7d398dddc692b81f426bca998425d6daeee81e5
Jonathan DeCastro <jonathan.decastro@tri.global>
2017-05-22 17:56:52 -0400

    WIP

--- drake/automotive/test/pose_selector_test.cc
+++ drake/automotive/test/pose_selector_test.cc
@@ -5,7 +5,6 @@
 #include "drake/automotive/maliput/api/lane.h"
 #include "drake/automotive/maliput/api/road_geometry.h"
 #include "drake/automotive/maliput/dragway/road_geometry.h"
-#include "drake/automotive/monolane_onramp_merge.h"
 
 namespace drake {
 namespace automotive {
@@ -32,16 +31,6 @@ constexpr int kFarAheadIndex{0};
 constexpr int kJustAheadIndex{1};
 constexpr int kJustBehindIndex{2};
 constexpr int kFarBehindIndex{3};
-
-static const maliput::api::Lane* GetLaneByJunctionId(
-    const maliput::api::RoadGeometry& road, const std::string& lane_id) {
-  for (int i = 0; i < road.num_junctions(); ++i) {
-    if (road.junction(i)->id().id == lane_id) {
-      return road.junction(i)->segment(0)->lane(0);
-    }
-  }
-  throw std::runtime_error("No matching junction name in the road network");
-}
 
 static void SetDefaultDragwayPoses(PoseVector<double>* ego_pose,
                                    FrameVelocity<double>* ego_velocity,
@@ -112,32 +101,6 @@ static void SetDefaultPosesSideBySide(PoseVector<double>* ego_pose,
       kTrafficXVelocity /* vx */, 0. /* vy */, 0. /* vz */;
   traffic_poses->set_pose(0, Eigen::Isometry3d(translation));
   traffic_poses->set_velocity(0, traffic_velocity);
-}
-
-static void SetDefaultOnrampPoses(const GeoPosition& traffic_geo_position,
-                                  const double traffic_vy,
-                                  PoseVector<double>* ego_pose,
-                                  FrameVelocity<double>* ego_velocity,
-                                  PoseBundle<double>* traffic_poses) {
-  DRAKE_DEMAND(traffic_poses->get_num_poses() == 1);
-  DRAKE_DEMAND(kEgoSPosition > kTrailingSPosition && kTrailingSPosition > 0.);
-
-  // Create poses for one ego vehicle in the "onramp0" lane.
-  ego_pose->set_translation(
-      Eigen::Translation3d(195. /* x */, -50. /* y */, 0. /* z */));
-  Vector6<double> velocity{};
-  velocity << 0. /* ωx */, 0. /* ωy */, 0. /* ωz */, 0. /* vx */, -10. /* vy */,
-      0. /* vz */;
-  ego_velocity->set_velocity(multibody::SpatialVelocity<double>(velocity));
-
-  const Eigen::Translation3d translation_ahead(
-      traffic_geo_position.x() /* x */, traffic_geo_position.y() /* y */,
-      traffic_geo_position.z() /* z */);
-  FrameVelocity<double> velocity_ahead{};
-  velocity_ahead.get_mutable_value() << 0. /* ωx */, 0. /* ωy */, 0. /* ωz */,
-      traffic_vy /* vx */, 0. /* vy */, 0. /* vz */;
-  traffic_poses->set_pose(0, Eigen::Isometry3d(translation_ahead));
-  traffic_poses->set_velocity(0, velocity_ahead);
 }
 
 GTEST_TEST(PoseSelectorTest, DragwayTest) {
@@ -300,73 +263,6 @@ GTEST_TEST(PoseSelectorTest, IdenticalSValues) {
   // classified as a trailing car.
   EXPECT_EQ(std::numeric_limits<double>::infinity(), leading_odometry.pos.s());
   EXPECT_EQ(kEgoSPosition, trailing_odometry.pos.s());
-}
-
-GTEST_TEST(PoseSelectorTest, OnrampMergeTest) {
-  // Instantiate the onramp merge road.
-  std::unique_ptr<MonolaneOnrampMerge> merge_example(new MonolaneOnrampMerge);
-  auto road = merge_example->BuildOnramp();
-
-  PoseVector<double> ego_pose;
-  FrameVelocity<double> ego_velocity;
-  PoseBundle<double> traffic_poses(1);
-
-  // Set a traffic car in lane "post5".
-  GeoPosition traffic_geo_post5{0. /* x */, 0. /* y */, 0. /* z */};
-
-  // Define the default poses.
-  SetDefaultOnrampPoses(traffic_geo_post5, 10 /* traffic_vy */, &ego_pose,
-                        &ego_velocity, &traffic_poses);
-
-  // Check that the ego car's pose is in "onramp0".
-  const GeoPosition ego_geo_position{ego_pose.get_translation().x(),
-                                     ego_pose.get_translation().y(),
-                                     ego_pose.get_translation().z()};
-  EXPECT_TRUE(PoseSelector::IsWithinLane(
-      ego_geo_position, GetLaneByJunctionId(*road, "j:onramp0")));
-
-  // Calculate the current road position and use it to determine the ego car's
-  // lane.
-  const GeoPosition geo_position{ego_pose.get_translation().x(),
-                                 ego_pose.get_translation().y(),
-                                 ego_pose.get_translation().z()};
-  const RoadPosition& ego_position =
-      road->ToRoadPosition(geo_position, nullptr, nullptr, nullptr);
-
-  RoadOdometry<double> leading_odometry{};
-  double headway_distance{};
-  leading_odometry = PoseSelector::FindSingleClosestPose(
-      ego_position.lane, ego_pose, ego_velocity, traffic_poses, 1000.,
-      WhichSide::kAhead, &headway_distance);
-
-  // Verifies that we are on the road and that the correct car was identified.
-  EXPECT_EQ(0., leading_odometry.pos.s());
-  EXPECT_NEAR(265.067, headway_distance, 1e-3);
-
-  // Now, look for cars within the default path and across the branch.
-  leading_odometry = PoseSelector::FindSingleClosestAheadAndInBranches(
-      *road, ego_pose, ego_velocity, traffic_poses,
-      1000. /* scan_ahead_distance */, &headway_distance);
-
-  // Verifies that we obtain the same result as above.
-  EXPECT_EQ(0., leading_odometry.pos.s());
-  EXPECT_NEAR(265.067, headway_distance, 1e-3);
-
-  // Set a traffic car in lane "pre0".
-  GeoPosition traffic_geo_pre0{180. /* x */, -155. /* y */, 0. /* z */};
-  // GeoPosition traffic_geo_pre0{0. /* x */, 0. /* y */, 0. /* z */};
-
-  // Define the default poses.
-  SetDefaultOnrampPoses(traffic_geo_pre0, -10 /* traffic_vy */, &ego_pose,
-                        &ego_velocity, &traffic_poses);
-
-  leading_odometry = PoseSelector::FindSingleClosestAheadAndInBranches(
-      *road, ego_pose, ego_velocity, traffic_poses,
-      1000 /* scan_ahead_distance */, &headway_distance);
-
-  // Verifies that we are on the road and that the correct car was identified.
-  EXPECT_NEAR(30.064, leading_odometry.pos.s(), 1e-3);
-  EXPECT_NEAR(95.003, headway_distance, 1e-3);
 }
 
 GTEST_TEST(PoseSelectorTest, TestGetIsoVelocity) {
