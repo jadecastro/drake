67fc9901597a85a3fd600be27bdf8a7b935421b7
Jon <jonathan.decastro@tri.global>
2017-08-30 10:36:19 -0400

    WIP linear MPC with fixed equilibrium

--- drake/systems/controllers/linear_model_predictive_controller.h
+++ drake/systems/controllers/linear_model_predictive_controller.h
@@ -1,7 +1,11 @@
 #pragma once
 
+
 #include "drake/common/drake_copyable.h"
+#include "drake/common/trajectories/piecewise_polynomial.h"
+#include "drake/common/trajectories/piecewise_polynomial_trajectory.h"
 #include "drake/systems/framework/leaf_system.h"
+#include "drake/systems/primitives/linear_system.h"
 
 namespace drake {
 namespace systems {
@@ -23,45 +27,48 @@ class LinearModelPredictiveController : public LeafSystem<T> {
   /// context must be an equilbrium point of the plant model.
   LinearModelPredictiveController(
       std::unique_ptr<systems::System<double>> model,
-      std::unique_ptr<systems::Context<double>> context,
+      std::unique_ptr<systems::Context<double>> base_context,
       const Eigen::MatrixXd& Q,
       const Eigen::MatrixXd& R,
-      double sampling_time,
+      double time_period,
       double time_horizon);
 
   LinearModelPredictiveController(
       std::unique_ptr<systems::System<double>> model,
-      const PiecewisePolynomialTrajectory& x0,
-      const PiecewisePolynomialTrajectory& u0,
+      std::unique_ptr<PiecewisePolynomialTrajectory> x0,
+      std::unique_ptr<PiecewisePolynomialTrajectory> u0,
       const Eigen::MatrixXd& Q,
       const Eigen::MatrixXd& R,
-      double sampling_time,
+      double time_period,
       double time_horizon);
 
-  const InputPortDescriptor<T>& get_state() const {
-    return this->get_input_port(input_index_state_);
+  const InputPortDescriptor<T>& get_state_port() const {
+    return this->get_input_port(state_input_index_);
   }
-  const OutputPort<T>& get_control() const {
-    return this->get_output_port(output_index_control_);
+  const OutputPort<T>& get_control_port() const {
+    return this->get_output_port(control_output_index_);
   }
 
  protected:
   void CalcControl(const Context<T>& context, BasicVector<T>* control) const;
 
  private:
-  const std::unique_ptr<systems::System<double>> model_;
-  const std::unique_ptr<systems::Context<double>> context_;
+  const int state_input_index_{-1};
+  const int control_output_index_{-1};
 
-  const PiecewisePolynomialTrajectory x0_;
-  const PiecewisePolynomialTrajectory u0_;
+  std::unique_ptr<systems::System<double>> model_;
+  std::unique_ptr<systems::Context<double>> base_context_;
 
-  const int input_index_state_{-1};
-  const int output_index_control_{-1};
+  std::unique_ptr<PiecewisePolynomialTrajectory> x0_;
+  std::unique_ptr<PiecewisePolynomialTrajectory> u0_;
 
-  const double sampling_time_{};
+  const Eigen::MatrixXd Q_;
+  const Eigen::MatrixXd R_;
+
+  const double time_period_{};
   const double time_horizon_{};
 
-  std::unique_ptr<systems::LinearSystem<double>> linear_model_;
+  std::unique_ptr<LinearSystem<double>> linear_model_;
 };
 
 }  // namespace controllers
