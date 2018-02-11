a545bdeff0f75fce1ff5bb55f6679cb226921286
Jon <jonathan.decastro@tri.global>
2018-02-11 12:55:36 -0500

    Add bindings for a subset of Automotive

---
+++ bindings/pydrake/automotive_py.cc
@@ -0,0 +1,76 @@
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+
+#include "drake/automotive/gen/driving_command.h"
+#include "drake/automotive/calc_ongoing_road_position.h"
+#include "drake/automotive/idm_controller.h"
+#include "drake/automotive/maliput/dragway/road_geometry.h"
+#include "drake/automotive/pure_pursuit_controller.h"
+#include "drake/automotive/simple_car.h"
+#include "drake/bindings/pydrake/pydrake_pybind.h"
+#include "drake/bindings/pydrake/systems/systems_pybind.h"
+#include "drake/systems/framework/basic_vector.h"
+
+namespace drake {
+namespace pydrake {
+
+PYBIND11_MODULE(automotive, m) {
+  // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
+  using namespace drake::systems;
+  // NOLINTNEXTLINE(build/namespaces): Emulate placement in namespace.
+  using namespace drake::automotive;
+
+  m.doc() = "Bindings for Automotive systems.";
+
+  py::module::import("pydrake.systems.framework");
+
+  // TODO(jadecastro) Bind AutoDiffXd.
+  // TODO(jadecastro) Bind symbolic::Expression.
+  using T = double;
+
+  using pysystems::DefClone;
+
+  py::enum_<RoadPositionStrategy>(m, "RoadPositionStrategy")
+      .value("kCache", RoadPositionStrategy::kCache)
+      .value("kExhaustiveSearch", RoadPositionStrategy::kExhaustiveSearch);
+
+  py::class_<LaneDirection>(m, "LaneDirection")
+      .def(py::init<const maliput::dragway::Lane*, bool>());
+  pysystems::AddValueInstantiation<LaneDirection>(m);
+
+  // TODO(jadecastro) How to write the below instantiation against
+  // "maliput::api"?
+  // Use BasicVector's approach to expose base class members here.
+  py::class_<IdmController<T>, LeafSystem<T>>(m, "IdmController")
+      .def(py::init<const maliput::dragway::RoadGeometry&,
+           RoadPositionStrategy, double>())
+      .def("ego_pose_input", &IdmController<T>::ego_pose_input)
+      .def("ego_velocity_input", &IdmController<T>::ego_velocity_input)
+      .def("traffic_input", &IdmController<T>::traffic_input);
+
+  py::class_<PurePursuitController<T>, LeafSystem<T>>(
+      m, "PurePursuitController")
+      .def(py::init<>())
+      .def("ego_pose_input", &PurePursuitController<T>::ego_pose_input)
+      .def("lane_input", &PurePursuitController<T>::lane_input)
+      .def("steering_command_output",
+           &PurePursuitController<T>::steering_command_output);
+
+  py::class_<SimpleCar<T>, LeafSystem<T>>(m, "SimpleCar")
+      .def(py::init<>())
+      .def("state_output", &SimpleCar<T>::state_output)
+      .def("pose_output", &SimpleCar<T>::pose_output)
+      .def("velocity_output", &SimpleCar<T>::velocity_output);
+
+  m.def("create_lane_direction",
+        [](const maliput::dragway::Lane* lane, bool with_s) {
+          return new LaneDirection(lane, with_s);
+        });
+
+  m.def("create_driving_command", []() -> BasicVector<T>* {
+      return new DrivingCommand<T>();
+    });
+}
+
+}  // namespace pydrake
+}  // namespace drake
---
+++ bindings/pydrake/test/automotive_test.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import copy
+import unittest
+import numpy as np
+
+import pydrake.systems.framework as framework
+from pydrake.automotive import (
+    LaneDirection,
+    IdmController,
+    PurePursuitController,
+    RoadPositionStrategy,
+    SimpleCar,
+    create_lane_direction,
+    create_driving_command,
+    )
+from pydrake.maliput.api import (
+    RoadGeometryId
+    )
+import pydrake.maliput.dragway as dragway
+from pydrake.systems.analysis import (
+    Simulator,
+    )
+from pydrake.systems.primitives import (
+    Multiplexer,
+    TrajectorySource,
+    )
+from pydrake.systems.rendering import (
+    PoseAggregator,
+    )
+from pydrake.trajectories import (
+    PiecewisePolynomialTrajectory,
+    create_zoh_piecewise_polynomial,
+    create_foh_piecewise_polynomial,
+    )
+
+
+# Construct a IDM-controlled SimpleCar in a dragway, a middle-ground between IDM
+# and MOBIL.  Essentially it's a redeaux of
+# AutomotiveSimulator::IdmControlledCar.
+class TestAutomotiveDiagram(unittest.TestCase):
+    def test_idm_dragway(self):
+        # Instantiate a two-lane straight road.
+        rg_id = RoadGeometryId("single-lane road")
+        rg = dragway.RoadGeometry(rg_id, 2, 100., 4., 0., 1., 1e-6, 1e-6)
+        segment = rg.junction(0).segment(0)
+        lane_0 = segment.lane(0)
+        lane_1 = segment.lane(1)
+
+        # Create a trajectory source system from some made-up trajectory data.
+        # (Note that we're not using TrajectoryCar, as it doesn't track velocity
+        # profiles.)
+        # TODO import NGSIM data.
+        breaks = [0., 1.]
+        knots = []
+        knots.append(np.array([5., 34.]))
+        knots.append(np.array([79., 42.]))
+        traject = create_foh_piecewise_polynomial(breaks, knots)
+        source = TrajectorySource(PiecewisePolynomialTrajectory(traject))
+        # TODO(jadecastro) Create PoseVector and FrameVelocity from this source;
+        # tie into PoseAggregator.
+
+        # Build a diagram with the IDM car placed in lane 0.
+        builder = framework.DiagramBuilder()
+
+        idm = builder.AddSystem(IdmController(rg, RoadPositionStrategy.kExhaustiveSearch, 0.))
+        simple_car = builder.AddSystem(SimpleCar())
+        pursuit = builder.AddSystem(PurePursuitController())
+        mux = builder.AddSystem(Multiplexer(create_driving_command()))
+        aggregator = builder.AddSystem(PoseAggregator())
+
+        # TODO(jadecastro) Use named port getters provided by each system.
+        builder.Connect(simple_car.get_output_port(1), idm.get_input_port(0))
+        builder.Connect(simple_car.get_output_port(2), idm.get_input_port(1))
+        builder.Connect(pursuit.get_output_port(0), mux.get_input_port(0))
+        builder.Connect(idm.get_output_port(0), mux.get_input_port(1))
+        builder.Connect(mux.get_output_port(0), simple_car.get_input_port(0))
+
+        ports = aggregator.AddSinglePoseAndVelocityInput("idm_car_0", 0)
+
+        builder.Connect(simple_car.get_output_port(1), ports[0])
+        builder.Connect(simple_car.get_output_port(2), ports[1])
+        builder.Connect(aggregator.get_output_port(0), idm.get_input_port(2))
+        builder.ExportOutput(aggregator.get_output_port(0))
+
+        builder.ExportInput(pursuit.get_input_port(0))
+
+        diagram = builder.Build()
+
+        # Fix the lane input for now.
+        context = diagram.CreateDefaultContext()
+        # value = framework.AbstractValue.Make(LaneDirection(lane_0, True))
+        value = framework.Value[LaneDirection](lane_0, True)
+        context.FixInputPort(0, value)
+
+        # Set up the simulator.
+        simulator = Simulator(diagram, context)
+        simulator.Initialize()
+
+        state = simulator.get_mutable_context().get_mutable_state() \
+                         .get_mutable_continuous_state().get_mutable_vector()
+        print(state.CopyToVector())
+
+        # initial_state = np.array([0., 0., 0., 0.])
+        # state.SetFromVector(initial_state)
+
+        # Run a simulation step. This can go into a loop so that we can access
+        # the rollout (or modify the context directly).
+        simulator.StepTo(1.)
+
+        print(state.CopyToVector())
+
+    def test_simple_car(self):
+        simple_car = SimpleCar()
+        command = create_driving_command()
+        value = command.get_mutable_value()
+        # TODO(jadecastro) Implement with named vectors.
+        value[0] = 1.
+        value[1] = 1.
+        print(command.get_value())
+
+        context = simple_car.CreateDefaultContext()
+        context.FixInputPort(0, command)
+
+        simulator = Simulator(simple_car, context)
+        simulator.Initialize()
+
+        state = simulator.get_mutable_context().get_mutable_state() \
+                         .get_mutable_continuous_state().get_mutable_vector()
+
+        # TODO(jadecastro) Used named vectors.
+        initial_state = np.array([0., 0., 0., 0.])
+        state.SetFromVector(initial_state)
+
+        simulator.StepTo(1.)
+
+        print(state.CopyToVector())
+
+
+if __name__ == '__main__':
+    unittest.main()
